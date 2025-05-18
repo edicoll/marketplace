2e03c2fefc79bd75d713bc6cd5097c1b6cbc2081
Edi <you@example.com>
2025-05-18 23:19:25 +0200

    Dodane su data klase i napravljen dizajn za profile fragment

--- app/src/main/java/com/example/projectmarketplace/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/projectmarketplace/fragments/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.projectmarketplace.fragments
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,29 +8,29 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RatingBar
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import com.example.projectmarketplace.R
 import com.example.projectmarketplace.data.User
 
 class ProfileFragment : Fragment() {
 
     private lateinit var currentUser: User
-    private lateinit var ratingBar: RatingBar
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout
+
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        // Dohvati korisnika
-        currentUser = arguments?.getParcelable("USER_KEY") ?: User(2, "ffh", "ww0,", 3F,"hh")
+        // dohvaćanje korisnika
+        currentUser = arguments?.getParcelable("USER_KEY", User::class.java) ?: User(2, "ffh", "ww0,", 3F, "hh")
 
-        // Pronađi view-ove i postavi vrijednosti
         val name = view.findViewById<TextView>(R.id.name)
         val email = view.findViewById<TextView>(R.id.email)
-        ratingBar = view.findViewById<RatingBar>(R.id.ratingBar)
+        val ratingBar = view.findViewById<RatingBar>(R.id.ratingBar)
 
         name.text = currentUser.name
         email.text = currentUser.email
