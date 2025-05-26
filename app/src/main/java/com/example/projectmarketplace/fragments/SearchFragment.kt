72b0f6742ada7910e871595e321d7411bc4469ae
Edi <you@example.com>
2025-05-26 19:27:20 +0200

    Izrada search fragmenta. Implementiran search sistem, prikaz proizvoda automatski kada se upišu više od 3 slova i otvaranje itema.

--- app/src/main/java/com/example/projectmarketplace/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/projectmarketplace/fragments/SearchFragment.kt
@@ -2,7 +2,6 @@ package com.example.projectmarketplace.fragments
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +10,6 @@ import android.view.inputmethod.EditorInfo
 import androidx.annotation.RequiresApi
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.projectmarketplace.R
 import com.example.projectmarketplace.adapters.SearchAdapter
 import com.example.projectmarketplace.data.Item
 import com.example.projectmarketplace.databinding.FragmentSearchBinding
