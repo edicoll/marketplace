e60b396a6c2f3969eb9974380fbea87dc489ec1e
Edi <you@example.com>
2025-06-04 11:58:33 +0200

    Izrađena funkcionalnost za prikazivanje review-ova na profilu

---
+++ app/src/main/java/com/example/projectmarketplace/adapters/ReviewAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.projectmarketplace.adapters
+
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projectmarketplace.R
+import com.example.projectmarketplace.data.Review
+
+
+class ReviewAdapter (private val reviews: List<Review>
+) : RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder>() {
+
+
+    //čuva podatke za svaki red liste
+    class ReviewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name: TextView = itemView.findViewById(R.id.name)
+        val text: TextView = itemView.findViewById(R.id.text)
+        val rating: RatingBar = itemView.findViewById(R.id.ratingBar)
+
+
+    }
+
+    //kreira se novi UI element, to jest review
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewAdapter.ReviewViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_review, parent, false)
+        return ReviewViewHolder(view)
+    }
+
+    //postavlja se nakon što se kreira item
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
+        val review = reviews[position]
+
+        holder.name.text = review.userIdFrom.toString()
+        holder.text.text = review.comment
+        holder.rating.rating = review.rating.toFloat()
+
+
+
+
+    }
+
+    override fun getItemCount() = reviews.size
+}
--- app/src/main/java/com/example/projectmarketplace/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/projectmarketplace/fragments/ProfileFragment.kt
@@ -6,15 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RatingBar
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import com.example.projectmarketplace.R
+import com.example.projectmarketplace.data.Review
 import com.example.projectmarketplace.data.User
+import com.example.projectmarketplace.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     private lateinit var currentUser: User
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+    private var reviews: List<Review> = emptyList()
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreateView(
@@ -22,21 +25,50 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
 
-        val view = inflater.inflate(R.layout.fragment_profile, container, false)
-
-        // dohvaćanje korisnika
+        // dohvaćanje podataka
         currentUser = arguments?.getParcelable("USER_KEY", User::class.java) ?: User(2, "ffh", "ww0,", 3F, "hh")
+        reviews = arguments?.getParcelableArrayList<Review>("REVIEW_KEY", Review::class.java) ?: emptyList()
 
-        val name = view.findViewById<TextView>(R.id.name)
-        val email = view.findViewById<TextView>(R.id.email)
-        val ratingBar = view.findViewById<RatingBar>(R.id.ratingBar)
+        val name = binding.name
+        val email = binding.email
+        val ratingBar = binding.ratingBar
 
         name.text = currentUser.name
         email.text = currentUser.email
         ratingBar.rating = currentUser.rating
 
+        return binding.root
+    }
 
-        return view
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupActionClickListener(binding.userInfoContainer)
+
+    }
+
+    private fun setupActionClickListener(view: View) {
+        view.setOnClickListener {
+            val reviewFragment = ReviewFragment()
+
+            val bundle = Bundle().apply {
+                putParcelable("USER_KEY", currentUser)
+                putParcelableArrayList("REVIEW_KEY", ArrayList(reviews))
+            }
+            reviewFragment.arguments = bundle
+
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.flFragment, reviewFragment)
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
